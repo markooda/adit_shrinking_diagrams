a8b5e9cfaccfbaca135e93dd5db7097de834ae75
sinthoras50 <sinthoras@hotmail.sk>
2025-10-24 16:20:47 +0200

    Add support for ts/tsx

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,10 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+// images
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
